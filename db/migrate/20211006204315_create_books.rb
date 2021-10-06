a2558662874b83d84a63bad07b7e7c2f532e260c
Codi Kraus <codi.kraus@gmail.com>
2021-10-06 15:53:06 -0500

    rails resource Book/Category generator

---
+++ db/migrate/20211006204315_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.string :image_url
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
