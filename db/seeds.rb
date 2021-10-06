4d669844960edc8054e68ce9f8eb4b651d0cfc3d
Codi Kraus <codi.kraus@gmail.com>
2021-10-06 15:53:15 -0500

    add seed data to databases

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+fantasy = Category.create(name:"Fantasy")
+sci_fi = Category.create(name:"Science Fiction")
+history = Category.create(name:"History")
+
+enders_game = Book.create(title:"Enders Game", author:"Orson Scott Card", description:"Kid soldier kills aliens", category_id:sci_fi.id)
+fellowship = Book.create(title:"Fellowship of the Ring", author:"J.R.R. Tolkien", description:"Hobbit is given impossible quest", category_id:fantasy.id)
+spqr = Book.create(title:"SPQR", author:"Mary Beard", description:"Historical account of the founding of Rome.", category_id:history.id)
