47b9c3d93b33748681d2544365c8da2a348f7bf7
Codi Kraus <codi.kraus@gmail.com>
2021-10-06 16:34:59 -0500

    update schema to include year pub. and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_204315) do
+ActiveRecord::Schema.define(version: 2021_10_06_212651) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_10_06_204315) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "year_published"
+    t.index ["category_id"], name: "index_books_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
