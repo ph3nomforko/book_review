17c0109c9928099642a8632e81a75ef45ba13849
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 09:45:35 -0500

    add index and create actions for books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::BooksController < ApplicationController
     def index
-        @books = Book.all
-        render json: @books
+        books = Book.all
+        render json: books
+    end
+
+    def create
+        book = Book.new(book_params)
+        if book.save
+            render json: book, status: :accepted
+        else
+            render json: {errors: book.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :description, :image_url, :year_published, :category_id)
     end
 end
