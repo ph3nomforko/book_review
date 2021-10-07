836ffdeae5974e16ab4f3784e4d29c7352d448fd
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 10:32:34 -0500

    remove non-api book and category controllers

--- app/controllers/books_controller.rb
+++
@@ -1,6 +0,0 @@
-class BooksController < ApplicationController
-    def index
-        books = Book.all
-        render json: books, include: [:category]
-    end
-end
--- app/controllers/categories_controller.rb
+++
@@ -1,6 +0,0 @@
-class CategoriesController < ApplicationController
-    def index
-        categories = Category.all
-        render json: categories, include: [:books]
-    end
-end
