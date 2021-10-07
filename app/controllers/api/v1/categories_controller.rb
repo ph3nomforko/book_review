9291829193ed65b6736698e98d726a1c517b5cec
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 10:29:06 -0500

    create categories api controller

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+        render json: CategorySerializer.new(categories)
+    end
+
+    def create
+        category = Category.new(category_params)
+        if category.save
+            render json: category, status: :accepted
+        else
+            render json: {errors: category.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+end
