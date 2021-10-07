14d8ee4be73709b5c0bb2f175fce8926fddfbc54
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 15:55:11 -0500

    add function to get books and display on index

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,22 @@
-// console.log("in index.js")
 const endPoint = "http://localhost:3000/api/v1/books"
+
 document.addEventListener('DOMContentLoaded', () => {
+    getBooks()
+})
+
+function getBooks() {
     fetch(endPoint)
     .then(response => response.json())
     .then(books => {
-        console.log(books)
+        books.data.forEach(book => {
+            const bookMarkup =
+            `<div data-id=${book.id}>
+              <img src=${book.attributes.image_url} width="300" height="350">
+              <h3>${book.attributes.title}</h3>
+              <p>${book.attributes.author}</p>
+              <p>${book.attributes.category.name}</p>
+            </div><br><br>`
+            document.querySelector('#book-container').innerHTML += bookMarkup
+        });
     })
-})
+}
