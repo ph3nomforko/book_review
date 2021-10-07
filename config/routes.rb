438b794fa1a09223a4d6ce06700c62e181c5cdc4
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 10:29:55 -0500

    add categories route to api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: [:index, :create]
+      resources :categories, only: [:index, :create]
     end
   end
 end
