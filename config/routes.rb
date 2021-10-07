54a3977bf90eeba411f144dfdd65ae58e9d911b4
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 09:20:26 -0500

    add namespace route for books and move books con

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :books
+  # resources :categories
+  namespace :api do
+    namespace :v1 do
+      resources :books, only [:index]
+    end
+  end
 end
