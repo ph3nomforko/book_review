2168e02b87ff764f16c1151640a9acf834322ecd
Codi Kraus <codi.kraus@gmail.com>
2021-10-07 10:28:53 -0500

    use serializer route for book index

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BooksController < ApplicationController
     def index
         books = Book.all
-        render json: books
+        render json: BookSerializer.new(books)
     end
 
     def create
