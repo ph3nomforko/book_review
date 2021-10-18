b8d42edd9336f29ecc5711572ff5616f101988cc
Codi Kraus <codi.kraus@gmail.com>
2021-10-18 15:01:32 -0500

    add post fetch request and render new book

--- frontend/index.js
+++ frontend/index.js
@@ -29,9 +29,25 @@ document.addEventListener('submit', (e) => {
     const imgUrlInput = document.querySelector("#input-url").value
     const publishingYearInput = document.querySelector("#input-published").value
     const categoryId = parseInt(document.querySelector("#categories").value)
-    postFetch(titleInput, authorInput, descriptionInput, imgUrlInput, publishingYearInput, categoryId)
+    postFetch(titleInput, authorInput, descriptionInput, publishingYearInput, imgUrlInput, categoryId)
 })
 
-function postFetch(title, author, description, image_url, category_id) {
-    
+function postFetch(title, author, description, year_published, image_url, category_id) {
+    const bodyData = {title, author, description, year_published, image_url, category_id}
+    fetch(endPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(book => {
+        const bookMarkup =
+        `<div data-id=${book.id}>
+              <img src=${book.image_url} width="300" height="350">
+              <h3>${book.title}</h3>
+              <p>${book.author}</p>
+              <p>${book.category.name}</p>
+            </div><br><br>`
+        document.querySelector('#book-container').innerHTML += bookMarkup
+    })
 }
