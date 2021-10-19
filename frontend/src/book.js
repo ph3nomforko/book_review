41a05e8ae9bdabc673d573bd1ca30302c650aa5b
Codi Kraus <codi.kraus@gmail.com>
2021-10-19 12:32:12 -0500

    add constructor to book class

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -1,3 +1,22 @@
 class Book {
-    
+    constructor(id, bookAttributes) {
+        this.id = id
+        this.title = bookAttributes.title
+        this.author = bookAttributes.author
+        this.description = bookAttributes.description
+        this.year_published = bookAttributes.year_published
+        this.image_url = bookAttributes.image_url
+        this.category = bookAttributes.category
+        Book.call.push(this)
+    }
+    //renderBookCard() {
+    //    return `
+    //        <div data-id=${this.id}>
+    //            <img src=${this.image_url} height="200" width="250">
+    //            <h3>${this.title}</h3>
+    //            <p>${this.category.name}</p>
+    //        </div><br><br>`
+    //}
 }
+
+Book.all = []
