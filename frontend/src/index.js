b76e4e65eb80ceaca011e32968ef9d16b3ca42ed
Codi Kraus <codi.kraus@gmail.com>
2021-10-19 12:51:25 -0500

    use oojs class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,10 @@ function getBooks() {
     fetch(endPoint)
     .then(response => response.json())
     .then(books => {
-        books.data.forEach(book => { render(book) })
+        books.data.forEach(book => {
+            let newBook = new Book(book, book.attributes)
+            render(book)
+        })
     })
 }
 
