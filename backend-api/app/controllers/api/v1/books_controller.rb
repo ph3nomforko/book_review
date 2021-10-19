489fc252a566a341e25408b971da2ee732341ee9
Codi Kraus <codi.kraus@gmail.com>
2021-10-19 13:47:31 -0500

    add serializer to new book in api

--- backend-api/app/controllers/api/v1/books_controller.rb
+++ backend-api/app/controllers/api/v1/books_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::BooksController < ApplicationController
     def create
         book = Book.new(book_params)
         if book.save
-            render json: book, status: :accepted
+            render json: BookSerializer.new(book), status: :accepted
         else
             render json: {errors: book.errors.full_messages}, status: :unprocessible_entity
         end
