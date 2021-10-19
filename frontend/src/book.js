e3481c032d78bfd867eb591d3b84883682eaf974
Codi Kraus <codi.kraus@gmail.com>
2021-10-19 13:31:48 -0500

    add render function to Book class

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -1,23 +1,22 @@
 class Book {
     constructor(book, bookAttributes) {
         this.id = book.id
-        debugger
         this.title = bookAttributes.title
         this.author = bookAttributes.author
         this.description = bookAttributes.description
         this.year_published = bookAttributes.year_published
         this.image_url = bookAttributes.image_url
         this.category = bookAttributes.category
-        //Book.call.push(this)
+        Book.all.push(this)
     }
-    //renderBookCard() {
-    //    return `
-    //        <div data-id=${this.id}>
-    //            <img src=${this.image_url} height="200" width="250">
-    //            <h3>${this.title}</h3>
-    //            <p>${this.category.name}</p>
-    //        </div><br><br>`
-    //}
+    renderBookCard() {
+        return `
+            <div data-id=${this.id}>
+                <img src=${this.image_url} height="200" width="250">
+                <h3>${this.title}</h3>
+                <p>${this.category.name}</p>
+            </div><br><br>`
+    }
 }
 
 Book.all = []
