47315b05f780622ce42709cbd18bed8fc02b956e
Codi Kraus <codi.kraus@gmail.com>
2021-10-20 16:36:17 -0500

    change render book to be based on bootstrap

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -11,11 +11,29 @@ class Book {
     }
     renderBookCard() {
         return `
-            <div data-id=${this.id}>
-                <img src=${this.image_url} height="200" width="250">
-                <h3>${this.title}</h3>
-                <p>${this.category.name}</p>
-            </div><br><br>`
+            <div class="col">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                <div class="card-body">
+                    <h5 class="card-title">${this.title}</h5>
+                    <p class="card-text">${this.author}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                    <small class="text-muted">9 mins</small>
+                </div>
+                </div>
+            </div>
+        </div>`
+        //
+        //    `<div data-id=${this.id}>
+        //       <img src=${this.image_url} height="200" width="250">
+        //        <h3>${this.title}</h3>
+        //        <p>${this.category.name}</p>
+        //   </div><br><br>`
     }
     static findById(id) {
         return this.all.find(book => book.id === id)
