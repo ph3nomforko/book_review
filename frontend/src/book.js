2678a13477f571c3ce95429bc6c27c6c75cf712d
Codi Kraus <codi.kraus@gmail.com>
2021-10-21 13:34:23 -0500

    remove extra button and change HTML for modal

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -19,8 +19,7 @@ class Book {
                         <p class="card-text">${this.author}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" id="modalButtons" data-bs-target="#bookModal" data-bs-whatever="@${this.id}">View</button>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" id="${this.id}" data-target="#bookModal">View</button>
                             </div>
                             <small class="text-muted">${this.category.name}</small>
                         </div>
@@ -37,11 +36,11 @@ class Book {
 
     renderBookModal() {
         return `
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="bookModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">${this.title}</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
