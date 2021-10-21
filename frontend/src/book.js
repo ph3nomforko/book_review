af227e47a7597b06d9dfff2852dd88cd9cd33d26
Codi Kraus <codi.kraus@gmail.com>
2021-10-21 11:02:08 -0500

    interpolate id to view button on card

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -19,7 +19,7 @@ class Book {
                         <p class="card-text">${this.author}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" id='book-button-${this.id}' class="btn btn-sm btn-outline-secondary">View</button>
                                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                             </div>
                             <small class="text-muted">${this.category.name}</small>
