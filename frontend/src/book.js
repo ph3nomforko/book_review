f1a03316266c66379244b45d94d3c005a92b16ab
Codi Kraus <codi.kraus@gmail.com>
2021-10-21 12:17:21 -0500

    remove logic from render and name modal button

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -19,7 +19,7 @@ class Book {
                         <p class="card-text">${this.author}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#bookModal" data-bs-whatever=${this.id}>View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" id="modalButtons" data-bs-target="#bookModal" data-bs-whatever="@${this.id}">View</button>
                                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                             </div>
                             <small class="text-muted">${this.category.name}</small>
@@ -36,33 +36,24 @@ class Book {
     }
 
     renderBookModal() {
-        let bookModal = document.getElementById('bookModal')
-        bookModal.addEventListener('show.bs.modal', (e) => {
-            let button = e.relatedTarget
-            let recipient = button.findById()
-        })
-        return `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
-      </button>
-      
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>`   
+        return `
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`   
     }
 
     static findById(id) {
