07f10c79a5be1791744ee89c4e872556d361546b
Codi Kraus <codi.kraus@gmail.com>
2021-10-22 16:06:20 -0500

    add comments routes

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :books, only: [:index, :create]
       resources :categories, only: [:index, :create]
+      resources :comments, only: [:index, :create]
     end
   end
 end
