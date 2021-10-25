8aa80c4532455919d550bd76e7ba60875f37fe6b
Codi Kraus <codi.kraus@gmail.com>
2021-10-25 14:06:14 -0500

    generate iterative comment HTML for modal

--- frontend/src/book.js
+++ frontend/src/book.js
@@ -7,6 +7,7 @@ class Book {
         this.year_published = book.attributes.year_published
         this.image_url = book.attributes.image_url
         this.category = book.attributes.category
+        this.comments = book.attributes.comments
         Book.all.push(this)
     }
     renderBookCard() {
@@ -18,7 +19,6 @@ class Book {
                         <div class="d-flex justify-content-between align-items-center">
                             <button class="btn btn-sm btn-outline-warning" type="button" data-bs-toggle="modal" data-bs-target="#bookModal" id="book-${this.id}">View Book</button>
                             <button class="btn btn-sm btn-outline-info" type="button" data-bs-toggle="modal" data-bs-target="#addCommentModal">Add Comment</button>
-                            <button class="btn btn-sm btn-outline-secondary" type="button" data-bs-toggle="modal" data-bs-target="#commentsModal" id="bookComments-${this.id}">View Comments</button>
                         </div>
                     </div>
                 </div>
@@ -26,6 +26,11 @@ class Book {
     }
 
     renderBookModal() {
+        let commentHtml = ""
+        this.comments.forEach(comment => {
+            commentHtml += `
+            <p>(${comment.id}) ${comment.content} by ${comment.username}</p>`
+        })
         return `
             <div class="modal-header">
                 <img src=${this.image_url}>
@@ -36,6 +41,8 @@ class Book {
                 <h6>Year Published: ${this.year_published}</h6>
                 <p>Genre: ${this.category.name}<br>
                 Sumamry: ${this.description}</p>
+                <h5>Comments</h5>
+                ${commentHtml}
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
