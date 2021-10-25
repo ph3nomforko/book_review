9983f3b94d197a46d9913cf3a325c49517f02ad4
Codi Kraus <codi.kraus@gmail.com>
2021-10-25 14:05:53 -0500

    remove OO Comment modal handler

--- frontend/src/comment.js
+++ frontend/src/comment.js
@@ -6,21 +6,6 @@ class Comment {
         this.book = comment.attributes.book
         Comment.all.push(this)
     }
-
-    renderCommentsModal() {
-        return `
-        <div class="modal-header">
-            <h3>${this.book.title}</h3>
-        </div>
-        <div class="modal-body">
-            <h6>Year Published: ${this.content}</h6>
-            <p>Genre: ${this.category.name}<br>
-            Sumamry: ${this.description}</p>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        </div>`
-    }
 }
 
 Comment.all = []
