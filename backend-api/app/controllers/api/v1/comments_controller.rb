e74a360a8cd5d8c51ed8069cca7a039b32d973d0
Codi Kraus <codi.kraus@gmail.com>
2021-10-25 10:57:13 -0500

    add create function to comment

--- backend-api/app/controllers/api/v1/comments_controller.rb
+++ backend-api/app/controllers/api/v1/comments_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::CommentsController < ApplicationController
         comments = Comment.all
         render json: CommentSerializer.new(comments)
     end
+
+    def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            render json: CommentSerializer.new(comment), status: :accepted
+        else
+            render json: {errors: comment.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :username, :book_id)
+    end
 end
