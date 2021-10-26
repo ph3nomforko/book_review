425e660b3d7d392504ccba9598134bcec39491fb
Codi Kraus <codi.kraus@gmail.com>
2021-10-26 10:03:07 -0500

    add validations

--- backend-api/app/models/book.rb
+++ backend-api/app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
     belongs_to :category
     has_many :comments
+    validates :title, :author, :description, :image_url, :category_id, presence: true
 end
--- backend-api/app/models/comment.rb
+++ backend-api/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :book
+  validates :content, :username, :book_id, presence: true
+  validates :username, uniqueness: true
 end
